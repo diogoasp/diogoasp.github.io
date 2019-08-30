06243c2b689852c32bb80826ea5b4ddc9b742d41
Diogo Alves <diogoasp13@gmail.com>
2019-08-30 16:13:10 -0300

    estudo pt2

--- P2/estudo/script.js
+++ P2/estudo/script.js
@@ -11,8 +11,6 @@ $(document).ready(function (){
         $.ajax({
             url: "https://my-json-server.typicode.com/diogoasp/diogoasp.github.io/publi/"+index,
             success: function(result){
-              console.log("changePubli dentro / index: "+index);
-              console.log("https://my-json-server.typicode.com/diogoasp/diogoasp.github.io/publi/"+index);
               $('#publi').empty();
               $('#publi').append(`<div><p>${result.text}</p></div>`);
             }
