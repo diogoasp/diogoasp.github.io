6fcd7c0c34070be82b971e9dbacb5ac6faa64ec4
Diogo Alves <diogoasp13@gmail.com>
2019-08-30 10:37:55 -0300

    Estudando-P2

--- aula10-2/script.js
+++ aula10-2/script.js
@@ -1,21 +1,33 @@
-var button = document.getElementById('b1');
-button.addEventListener('click', addElement, false);
-
 function addElement(){
-  var text = document.getElementById('text');
-  var newElement = document.createElement('li');
-  newElement.textContent = text.value;
-  var ul = document.querySelector('ul');
-  ul.appendChild(newElement);
+  var text = document.getElementById('text').value;
+  if(text.length > 0){
+    var newElement = document.createElement('li');
+    newElement.textContent = text;
+    var ul = document.querySelector('ul');
+    ul.appendChild(newElement);
+  }else alert("Entrada de texto vazia.");
+}
+document.getElementById('b1').addEventListener('click', addElement, false);
+
+function turnLight(){
+  if(this.src.includes('off')){
+    this.src = "./img/lampada_on.gif";
+  }else this.src = "./img/lampada_off.gif";
 }
 
-var img = document.getElementById('img');
-img.addEventListener('click', lampada, false);
-
-function lampada(){
-  if(this.src.includes('off')){
-    this.src = "./lampada_on.gif";
-  }else{
-    this.src = "./lampada_off.gif";
+function breakLampada(){
+  if(!this.src.includes('broken')){
+    this.src = "./img/lampada_broken.gif";
+    this.removeEventListener('click', turnLight, false);
   }
 }
+
+function fixLampada(){
+  if(this.src.includes('broken')){
+    this.src = "./img/lampada_off.gif";
+    this.addEventListener('click', turnLight, false);
+  }
+}
+document.getElementById('img').addEventListener('click', turnLight, false);
+document.getElementById('img').addEventListener('dblclick', breakLampada, false);
+document.getElementById('img').addEventListener('mousewheel', fixLampada, false);
--- aula10/script.js
+++ aula10/script.js
@@ -1,26 +1,48 @@
-let action1 = document.getElementById('enlace_1');
-action1.addEventListener('click',() => {
-  ocultarElemento(action1, 'conteudo_1');
-},false);
+// let action1 = document.getElementById('enlace_1');
+// action1.addEventListener('click',() => {
+//   ocultarElemento(action1, 'conteudo_1');
+// },false);
+//
+// let action2 = document.getElementById('enlace_2');
+// action2.addEventListener('click',() => {
+//   ocultarElemento(action2, 'conteudo_2');
+// },false);
+//
+// let action3 = document.getElementById('enlace_3');
+// action3.addEventListener('click',() => {
+//   ocultarElemento(action3, 'conteudo_3');
+// },false);
+//
+// function ocultarElemento(action, id){
+//   let element = document.getElementById(id);
+//   var display = element.style.display;
+//   if(display == "none"){
+//     element.style.display = 'block';
+//     action.textContent = 'Ocultar Conteúdo'
+//   }else {
+//     element.style.display = 'none';
+//     action.textContent = 'Mostrar Conteúdo'
+//   }
+// }
 
-let action2 = document.getElementById('enlace_2');
-action2.addEventListener('click',() => {
-  ocultarElemento(action2, 'conteudo_2');
-},false);
 
-let action3 = document.getElementById('enlace_3');
-action3.addEventListener('click',() => {
-  ocultarElemento(action3, 'conteudo_3');
-},false);
-
-function ocultarElemento(action, id){
-  let element = document.getElementById(id);
-  var display = element.style.display;
-  if(display == "none"){
-    element.style.display = 'block';
-    action.textContent = 'Ocultar Conteúdo'
-  }else {
-    element.style.display = 'none';
-    action.textContent = 'Mostrar Conteúdo'
+//Fazendo direito!
+function hiddenElements(){
+  var element = this.previousElementSibling;
+  if(element.hidden == false){
+    element.hidden = true;
+    this.textContent = 'Mostrar Conteúdo';
+  }else{
+    element.hidden = false;
+    this.textContent = 'Ocultar Conteúdo';
   }
 }
+
+let action1 = document.getElementById('enlace_1');
+action1.addEventListener("click", hiddenElements, false);
+
+let action2 = document.getElementById('enlace_2');
+action2.addEventListener("click", hiddenElements, false);
+
+let action3 = document.getElementById('enlace_3');
+action3.addEventListener("click", hiddenElements, false);
