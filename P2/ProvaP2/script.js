a66e6a964f4ab9390bdecd977dcfcff8b1be160f
Diogo Alves <diogoasp13@gmail.com>
2019-08-30 23:14:21 -0300

    att minima do desespero
    Atualização na esperança de não deixar tão feio.

--- P2/ProvaP2/script.js
+++ P2/ProvaP2/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-
+  function showQuartos() {
+      if($('#nQuartos').val() == 2){
+          $('#2quarto').show();
+          $('#3quarto').hide();
+      }
+      if($('#nQuartos').val() == 3){
+        $('#2quarto').show();
+        $('#3quarto').show();
+      }
+      if($('#nQuartos').val() == 1){
+        $('#2quarto').hide();
+        $('#3quarto').hide();
+      }
+  };
   function calculate(){
     var dtChegada = new Date($('#dataChegada').val());
     var dtSaida = new Date($('#dataSaida').val());
@@ -7,29 +20,30 @@ $(document).ready(function(){
     const DIA = 24 * 60 * 60 * 1000;
     var qtdDias = (dtSaida.getTime()-dtChegada.getTime())/DIA;
     var valorCriancas = $('#qtdCrianca').val()*30.0;
-    var nQuartos = $("input[name='quartos']:checked").val();
-    var nQuartos2 = $("input[name='quartos2']:checked").val();
-    var nQuartos3 = $("input[name='quartos3']:checked").val()
+    var tQuartos = $("input[name='quartos1']:checked").val();
+    var tQuartos2 = $("input[name='quartos2']:checked").val();
+    var tQuartos3 = $("input[name='quartos3']:checked").val()
     var valorTotal = 0;
+
     // Tentei o Switch Case mas não rolou
+    if(tQuartos == 1) valorTotal += 139.00;
+    if(tQuartos == 2) valorTotal += 220.00;
+    if(tQuartos == 3) valorTotal += 340.00;
 
-    if(nQuartos == 1) valorTotal += 139.00;
-    if(nQuartos == 2) valorTotal += 220.00;
-    if(nQuartos == 3) valorTotal += 240.00;
+    if(tQuartos2 == 1) valorTotal += 139.00;
+    if(tQuartos2 == 2) valorTotal += 220.00;
+    if(tQuartos2 == 3) valorTotal += 340.00;
 
-    if(nQuartos2 == 1) valorTotal += 139.00;
-    if(nQuartos2 == 2) valorTotal += 220.00;
-    if(nQuartos2 == 3) valorTotal += 240.00;
-
-    if(nQuartos3 == 1) valorTotal += 139.00;
-    if(nQuartos3 == 2) valorTotal += 220.00;
-    if(nQuartos3 == 3) valorTotal += 240.00;
+    if(tQuartos3 == 1) valorTotal += 139.00;
+    if(tQuartos3 == 2) valorTotal += 220.00;
+    if(tQuartos3 == 3) valorTotal += 340.00;
 
     valorTotal += valorCriancas;
     valorTotal *= qtdDias;
-    $('#final').empty();
     var nome = $('#nome').val();
-    $('#final').append('<h3>Resumo: </h3><p>Nome:'+nome+'</p><p>Data Chegada:'+dtChegada+'</p><p>Data Saida: '+dtSaida+'</p><p>Valor:'+valorTotal+'</p>');
+
+    $('#final').empty();
+    $('#final').append('<h3>Resumo: </h3><p>Nome:'+nome+'</p><p>Data Chegada:'+dtChegada+'</p><p>Data Saida: '+dtSaida+'</p><p>Valor: R$'+valorTotal+'</p>');
     $(this).prev('.form').append('<input type="submit" name="" value="Reservar" id="btnSubmit">');
   };
 
@@ -40,7 +54,7 @@ $(document).ready(function(){
     }else $('#divCrianca').hide();
   }
 
-
+  $('#seQuartos').click(showQuartos);
   $('#crianca').click(detalheCrianca);
   $('#btnCalcular').click(calculate);
 });
