11580f288bf3b2a6180535ce0c20040e02f3bf15
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2019-08-30 21:42:27 -0300

    prova p2

---
+++ P2/ProvaP2/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  function calculate(){
+    var dtChegada = new Date($('#dataChegada').val());
+    var dtSaida = new Date($('#dataSaida').val());
+    // Quantidade de milisegundos em um dia
+    const DIA = 24 * 60 * 60 * 1000;
+    var qtdDias = (dtSaida.getTime()-dtChegada.getTime())/DIA;
+    var valorCriancas = $('#qtdCrianca').val()*30.0;
+    var nQuartos = $("input[name='quartos']:checked").val();
+    var valorTotal;
+    // Tentei o Switch Case mas não rolou
+    if(nQuartos == 1) valorTotal = 139.00;
+    if(nQuartos == 2) valorTotal = 220.00;
+    if(nQuartos == 3) valorTotal = 240.00;
+    valorTotal += valorCriancas;
+    $('#final').empty();
+    var nome = $('#nome').val();
+    $('#final').append('<h3>Resumo: </h3><p>Nome:'+nome+'</p><p>Data Chegada:'+dtChegada+'</p><p>Data Saida: '+dtSaida+'</p><p>Valor:'+valorTotal+'</p>');
+    $(this).prev('.form').append('<input type="submit" name="" value="Reservar" id="btnSubmit">');
+  };
+
+  function detalheCrianca(){
+    var element = $('#divCrianca');
+    if(element.is(':hidden')){
+      $('#divCrianca').show();
+    }else $('#divCrianca').hide();
+  }
+
+
+  $('#crianca').click(detalheCrianca);
+  $('#btnCalcular').click(calculate);
+});
