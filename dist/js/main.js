24197446be698526568181c74696f77571ccfe1e
Diogo Alves <diogoasp13@gmail.com>
2023-10-24 13:36:40 -0300

    update nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,11 @@
 // $('.carousel').carousel()
+
+let links = document.getElementsByClassName("nav-link");
+Array.from(links).forEach((link) =>{
+    link.addEventListener('click' , () => {
+        Array.from(links).forEach((l) =>{
+            l.classList.remove('active');
+        });
+        link.classList.add('active');
+    }, false);
+});
