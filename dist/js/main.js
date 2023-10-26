545113193fa38760796f9b989ad0e55b43dc570b
Diogo Alves <diogoasp13@gmail.com>
2023-10-26 00:57:28 -0300

    add project

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,110 +1,114 @@
-
-let links = document.getElementsByClassName("nav-link");
-Array.from(links).forEach((link) =>{
-    link.addEventListener('click' , () => {
-        Array.from(links).forEach((l) =>{
-            l.classList.remove('active');
-        });
-        link.classList.add('active');
-    }, false);
+let links = document.getElementsByClassName('nav-link');
+Array.from(links).forEach((link) => {
+  link.addEventListener(
+    'click',
+    () => {
+      Array.from(links).forEach((l) => {
+        l.classList.remove('active');
+      });
+      link.classList.add('active');
+    },
+    false,
+  );
 });
 
 function show_mail() {
-    document.getElementById('send-email-box').classList.remove('d-none');
-    document.getElementById('send-email-btn').classList.add('d-none');
+  document.getElementById('send-email-box').classList.remove('d-none');
+  document.getElementById('send-email-btn').classList.add('d-none');
 }
 
 function hide_mail() {
-    document.getElementById('send-email-box').classList.add('d-none');
-    document.getElementById('send-email-btn').classList.remove('d-none');
+  document.getElementById('send-email-box').classList.add('d-none');
+  document.getElementById('send-email-btn').classList.remove('d-none');
 }
 
 function updateSize() {
-    //define max options by screen size
-    if (window.innerWidth > 769) {
-        q = 3;
-    } else if(window.innerWidth > 460){
-        q = 2;
-    } else {
-        q = 1;
-    }
-    fill_carousel(q);
+  let s_width = window.innerWidth;
+  //define max options by screen size
+  if (s_width > 960) {
+    q = 3;
+  } else if (s_width > 686) {
+    q = 2;
+  } else {
+    q = 1;
+  }
+  fill_carousel(q);
 }
 // Refactor when possible
 function fill_carousel(q) {
-    let carousel = document.getElementById('carousel-options');
-    carousel.innerHTML = '';
-    let indicator_list = document.getElementById("carousel-indicators");
-    indicator_list.innerHTML = '';
-    document.getElementById('loading-gif').classList.remove('d-none')
-    let i = 0;
-    let q_carousel_item = 0
-    if (projects_dict.length > q) {
-        q_carousel_item = Math.ceil(projects_dict.length / q)
-    } else {
-        q_carousel_item = q = projects_dict.length
-    }
-    for (let j = 0; j < q_carousel_item; j++) {
+  let carousel = document.getElementById('carousel-options');
+  carousel.innerHTML = '';
+  let indicator_list = document.getElementById('carousel-indicators');
+  indicator_list.innerHTML = '';
+  document.getElementById('loading-gif').classList.remove('d-none');
+  let i = 0;
+  let q_carousel_item = 0;
+  if (projects_dict.length > q) {
+    q_carousel_item = Math.ceil(projects_dict.length / q);
+  } else {
+    q_carousel_item = q = projects_dict.length;
+  }
+  for (let j = 0; j < q_carousel_item; j++) {
+    let indicator = document.createElement('li');
+    indicator.setAttribute('data-target', '#carouselExampleIndicators');
+    indicator.setAttribute('ddata-slide-to', j);
 
-        let indicator = document.createElement('li');
-        indicator.setAttribute('data-target',"#carouselExampleIndicators");
-        indicator.setAttribute('ddata-slide-to',j);
-        
-        let carousel_item = document.createElement('div');
-        carousel_item.classList.add('carousel-item');
-        if (j == 0) {
-            carousel_item.classList.add('active');
-            indicator.classList.add('active')
-        }
-        
-        let div = document.createElement('div');
-        div.classList.add('d-flex');
-        indicator_list.appendChild(indicator);
-        
-        for (let k = 0; k < q; k++) {
-            if (projects_dict[i] == undefined) {
-                projects_dict[i] = {
-                    "name":"",
-                    "description":"",
-                    "image":"dist/images/none.png",
-                    "link":"#"
-                }
-            }
-            let carousel_link = document.createElement('a');
-            carousel_link.classList.add('carousel-link', 'mb-4');
-            if (k != q-1) {
-                carousel_link.classList.add('mr-2');
-            }
-            carousel_link.href = "projetos/"+projects_dict[i].link;
-            
-            let img = document.createElement('img');
-            img.classList.add('d-block', 'w-100');
-            img.src = projects_dict[i].image +'?auto=yes&bg=777&fg=555&text='+i+' Link';
-            img.alt = "";
-            carousel_link.appendChild(img);
-            
-            let caption = document.createElement('div');
-            caption.classList.add('carousel-caption', 'd-md-block');
-            
-            let name = document.createElement('h5');
-            name.innerHTML = projects_dict[i].name
-            let description = document.createElement('p');
-            description.innerHTML = projects_dict[i].description
-            
-            caption.appendChild(name);
-            caption.appendChild(description);
-            
-            carousel_link.appendChild(caption);
-            
-            div.appendChild(carousel_link);
-            i++;
-        }
-        
-        carousel_item.appendChild(div);
-        carousel.appendChild(carousel_item)
+    let carousel_item = document.createElement('div');
+    carousel_item.classList.add('carousel-item');
+    if (j == 0) {
+      carousel_item.classList.add('active');
+      indicator.classList.add('active');
     }
-    document.getElementById('loading-gif').classList.add('d-none')
+
+    let div = document.createElement('div');
+    div.classList.add('d-flex');
+    indicator_list.appendChild(indicator);
+
+    for (let k = 0; k < q; k++) {
+      if (projects_dict[i] == undefined) {
+        projects_dict[i] = {
+          name: '',
+          description: '',
+          image: 'dist/images/none.png',
+          link: '#',
+        };
+      }
+      let carousel_link = document.createElement('a');
+      carousel_link.classList.add('carousel-link', 'mb-4');
+      if (k != q - 1) {
+        carousel_link.classList.add('mr-2');
+      }
+      carousel_link.href = 'projetos/' + projects_dict[i].link;
+
+      let img = document.createElement('img');
+      img.classList.add('d-block', 'w-100');
+      img.src = projects_dict[i].image + '?auto=yes&bg=777&fg=555&text=' + i + ' Link';
+      img.alt = '';
+      carousel_link.appendChild(img);
+
+      let caption = document.createElement('div');
+      caption.classList.add('carousel-caption', 'd-md-block');
+
+      let name = document.createElement('h5');
+      name.innerHTML = projects_dict[i].name;
+      let description = document.createElement('p');
+      description.innerHTML = projects_dict[i].description;
+
+      caption.appendChild(name);
+      caption.appendChild(description);
+
+      carousel_link.appendChild(caption);
+
+      div.appendChild(carousel_link);
+      i++;
+    }
+
+    carousel_item.appendChild(div);
+    carousel.appendChild(carousel_item);
+  }
+  document.getElementById('loading-gif').classList.add('d-none');
 }
 
 updateSize();
-window.addEventListener("resize", updateSize);
+window.onresize = updateSize;
+// window.addEventListener('resize', updateSize);
--- dist/projects/data.js
+++ dist/projects/data.js
@@ -1,45 +1,44 @@
 var projects_dict = [
-    {
-        "name":"Cartão Goitacá",
-        "description":"Análise dos dados do projeto social Cartão Goitacá",
-        "image":"dist/projects/images/cartao-goitaca.jpg",
-        "link":"cartao_goitaca"
-    }
-    ,
-    {
-        "name":"Transparência Portal PAE",
-        "description":"Um projeto BI para apresentar os dados de transparência da educação",
-        "image":"dist/projects/images/portal_pae.png",
-        "link":"portal_pae"
-    },
-    {
-        "name":"Relatórios Gerenciais",
-        "description":"Um conjunto de relatórios com dados contábeis da Prefeitura Municipal de Campos dos Goytacazes",
-        "image":"dist/projects/images/relatorios_gerenciais.png",
-        "link":"relatorios_gerenciais"
-    },
-    {
-        "name":"Relatório de Custeio",
-        "description":"Análise dos dados de Custeio da Prefeitura Municipal de Campos dos Goytacazes",
-        "image":"dist/projects/images/relatorio_custeio.png",
-        "link":"relatorio_custeio"
-    },
-    {
-        "name":"Simulador de Impacto em Folha de Pagamento",
-        "description":"Calculadora de impacto orçamentário de reajuste salarial de servidores da Prefeitura Municipal de Campos dos Goytacazes",
-        "image":"dist/projects/images/simulador_impacto_folha.png",
-        "link":"simulador_impacto_folha"
-    },
-    {
-        "name":"Sistema de Suporte à Base de Custos",
-        "description":"Aplicação Web destinada a auxiliar nas operações de interação com os dados de custeio da Prefeitura Municipal de Campos dos Goytacazes",
-        "image":"dist/projects/images/ssbc.png",
-        "link":"ssbc"
-    },
-    {
-        "name":"Impacto de Ajuste Salarial de Professores",
-        "description":"Estudo Dinâmico de Impacto de Ajuste Salarial de Professores na Folha de Pagamento da Prefeitura Municipal de Campos dos Goytacazes",
-        "image":"dist/projects/images/impacto_ajuste_salarial_professores.png",
-        "link":"impacto_ajuste_salarial_professores"
-    }
-]
+  {
+    name: 'Cartão Goitacá',
+    description: 'Análise dos dados do projeto social Cartão Goitacá',
+    image: 'dist/projects/images/cartao-goitaca.jpg',
+    link: 'cartao_goitaca',
+  },
+  {
+    name: 'Transparência Portal PAE',
+    description: 'Um projeto BI para apresentar os dados de transparência da educação',
+    image: 'dist/projects/images/portal_pae.jpg',
+    link: 'portal_pae',
+  },
+  {
+    name: 'Relatórios Gerenciais',
+    description: 'Um conjunto de relatórios com dados contábeis da Prefeitura Municipal de Campos dos Goytacazes',
+    image: 'dist/projects/images/relatorios_gerenciais.png',
+    link: 'relatorios_gerenciais',
+  },
+  {
+    name: 'Relatório de Custeio',
+    description: 'Análise dos dados de Custeio da Prefeitura Municipal de Campos dos Goytacazes',
+    image: 'dist/projects/images/relatorio_custeio.png',
+    link: 'relatorio_custeio',
+  },
+  {
+    name: 'Simulador de Impacto em Folha de Pagamento',
+    description: 'Calculadora de impacto orçamentário de reajuste salarial de servidores da Prefeitura Municipal de Campos dos Goytacazes',
+    image: 'dist/projects/images/simulador_impacto_folha.png',
+    link: 'simulador_impacto_folha',
+  },
+  {
+    name: 'Sistema de Suporte à Base de Custos',
+    description: 'Aplicação Web destinada a auxiliar nas operações de interação com os dados de custeio da Prefeitura Municipal de Campos dos Goytacazes',
+    image: 'dist/projects/images/ssbc.png',
+    link: 'ssbc',
+  },
+  {
+    name: 'Impacto de Ajuste Salarial de Professores',
+    description: 'Estudo Dinâmico de Impacto de Ajuste Salarial de Professores na Folha de Pagamento da Prefeitura Municipal de Campos dos Goytacazes',
+    image: 'dist/projects/images/impacto_ajuste_salarial_professores.png',
+    link: 'impacto_ajuste_salarial_professores',
+  },
+];
